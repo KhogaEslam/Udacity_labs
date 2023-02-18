89ef8f918e6c5d23c631c478cc9091bd793db17e
Eslam G. Khoga <KhogaEslam@gmail.com>
2023-02-18 08:27:34 +0200

    formatting

--- Week 14/memory game/memory game/puplic/main.js
+++ Week 14/memory game/memory game/puplic/main.js
@@ -1,26 +1,21 @@
 // Select The Start Game Button
 document.querySelector(".control-buttons span").onclick = function () {
+  // Prompt Window To Ask For Name
+  let yourName = prompt("Whats Your Name?");
 
-    // Prompt Window To Ask For Name
-    let yourName = prompt("Whats Your Name?");
+  // If Name Is Empty
+  if (yourName == null || yourName == "") {
+    // Set Name To Unknown
+    document.querySelector(".name span").innerHTML = "Unknown";
 
-    // If Name Is Empty
-    if (yourName == null || yourName == "") {
+    // Name Is Not Empty
+  } else {
+    // Set Name To Your Name
+    document.querySelector(".name span").innerHTML = yourName;
+  }
 
-        // Set Name To Unknown
-        document.querySelector(".name span").innerHTML = 'Unknown';
-
-        // Name Is Not Empty
-    } else {
-
-        // Set Name To Your Name
-        document.querySelector(".name span").innerHTML = yourName;
-
-    }
-
-    // Remove Splash Screen
-    document.querySelector(".control-buttons").remove();
-
+  // Remove Splash Screen
+  document.querySelector(".control-buttons").remove();
 };
 
 // Effect Duration
@@ -43,123 +38,101 @@ shuffle(orderRange);
 
 // Add Order Css Property To Game Blocks
 blocks.forEach((block, index) => {
+  // Add CSS Order Property
+  block.style.order = orderRange[index];
 
-    // Add CSS Order Property
-    block.style.order = orderRange[index];
-
-    // Add Click Event
-    block.addEventListener('click', function () {
-
-        // Trigger The Flip Block Function
-        flipBlock(block);
-
-    });
-
+  // Add Click Event
+  block.addEventListener("click", function () {
+    // Trigger The Flip Block Function
+    flipBlock(block);
+  });
 });
 
 // Flip Block Function
 function flipBlock(selectedBlock) {
+  // Add Class is-flipped
+  selectedBlock.classList.add("is-flipped");
 
-    // Add Class is-flipped
-    selectedBlock.classList.add('is-flipped');
+  // Collect All Flipped Cards
+  let allFlippedBlocks = blocks.filter((flippedBlock) =>
+    flippedBlock.classList.contains("is-flipped")
+  );
 
-    // Collect All Flipped Cards
-    let allFlippedBlocks = blocks.filter(flippedBlock => flippedBlock.classList.contains('is-flipped'));
+  // If Theres Two Selected Blocks
+  if (allFlippedBlocks.length === 2) {
+    // console.log('Two Flipped Blocks Selected');
 
-    // If Theres Two Selected Blocks
-    if (allFlippedBlocks.length === 2) {
+    // Stop Clicking Function
+    stopClicking();
 
-        // console.log('Two Flipped Blocks Selected');
-
-        // Stop Clicking Function
-        stopClicking();
-
-        // Check Matched Block Function
-        checkMatchedBlocks(allFlippedBlocks[0], allFlippedBlocks[1]);
-
-    }
-
+    // Check Matched Block Function
+    checkMatchedBlocks(allFlippedBlocks[0], allFlippedBlocks[1]);
+  }
 }
 
 // Stop Clicking Function
 function stopClicking() {
+  // Add Class No Clicking on Main Container
+  blocksContainer.classList.add("no-clicking");
 
-    // Add Class No Clicking on Main Container
-    blocksContainer.classList.add('no-clicking');
-
-    // Wait Duration
-    setTimeout(() => {
-
-        // Remove Class No Clicking After The Duration
-        blocksContainer.classList.remove('no-clicking');
-
-    }, duration);
-
+  // Wait Duration
+  setTimeout(() => {
+    // Remove Class No Clicking After The Duration
+    blocksContainer.classList.remove("no-clicking");
+  }, duration);
 }
 
 // Check Matched Block
 function checkMatchedBlocks(firstBlock, secondBlock) {
+  let triesElement = document.querySelector(".tries span");
 
-    let triesElement = document.querySelector('.tries span');
+  if (firstBlock.dataset.technology === secondBlock.dataset.technology) {
+    firstBlock.classList.remove("is-flipped");
+    secondBlock.classList.remove("is-flipped");
 
-    if (firstBlock.dataset.technology === secondBlock.dataset.technology) {
+    firstBlock.classList.add("has-match");
+    secondBlock.classList.add("has-match");
 
-        firstBlock.classList.remove('is-flipped');
-        secondBlock.classList.remove('is-flipped');
+    document.getElementById("success").play();
+  } else {
+    triesElement.innerHTML = parseInt(triesElement.innerHTML) + 1;
 
-        firstBlock.classList.add('has-match');
-        secondBlock.classList.add('has-match');
-
-        document.getElementById('success').play()
-
-    } else {
-
-        triesElement.innerHTML = parseInt(triesElement.innerHTML) + 1;
-
-        setTimeout(() => {
-
-            firstBlock.classList.remove('is-flipped');
-            secondBlock.classList.remove('is-flipped');
-
-        }, duration);
-        document.getElementById('fail').play();
-
-    }
-
+    setTimeout(() => {
+      firstBlock.classList.remove("is-flipped");
+      secondBlock.classList.remove("is-flipped");
+    }, duration);
+    document.getElementById("fail").play();
+  }
 }
 
 // Shuffle Function
 function shuffle(array) {
+  // Settings Vars
+  let current = array.length,
+    temp,
+    random;
 
-    // Settings Vars
-    let current = array.length,
-        temp,
-        random;
+  while (current > 0) {
+    // Get Random Number
+    random = Math.floor(Math.random() * current);
 
-    while (current > 0) {
+    // Decrease Length By One
+    current--;
 
-        // Get Random Number
-        random = Math.floor(Math.random() * current);
+    // [1] Save Current Element in Stash
+    temp = array[current];
 
-        // Decrease Length By One
-        current--;
+    // [2] Current Element = Random Element
+    array[current] = array[random];
 
-        // [1] Save Current Element in Stash
-        temp = array[current];
+    // [3] Random Element = Get Element From Stash
+    array[random] = temp;
+  }
 
-        // [2] Current Element = Random Element
-        array[current] = array[random];
-
-        // [3] Random Element = Get Element From Stash
-        array[random] = temp;
-
-    }
-
-    return array;
-
+  return array;
 }
 
-  // Current Array [9, 2, 10, 4, 5, 6, 7, 3, 1, 8]
+// Current Array [9, 2, 10, 4, 5, 6, 7, 3, 1, 8]
 /*
   [1] Save Current Element in Stash
   [2] Current Element = Random Element
